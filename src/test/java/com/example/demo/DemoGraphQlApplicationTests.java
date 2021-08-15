183e741ded033b599efcb1be60ab60c5dd68ba84
thangarajalagarsamy <talagarsamy@cloudhealthtech.com>
2021-08-15 12:30:21 +0530

    simple-graphql

--- src/test/java/com/example/demo/DemoGraphQlApplicationTests.java
+++ src/test/java/com/example/demo/DemoGraphQlApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoGraphQlApplicationTests {
 
-/*	@Test
-	public void contextLoads() {
-	}*/
+    @Test
+    public void contextLoads() {
+    }
 
 }
